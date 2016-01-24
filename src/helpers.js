c655f0eccf29a1af1996307d36dd817a6431991f
Julien Viala <dev.vialajulien@gmail.com>
2016-01-24 20:15:25 +0100

    untested dirty draft

---
+++ src/helpers.js
@@ -0,0 +1,60 @@
+import http from 'superagent';
+import qs from 'qs';
+
+export const buildEndpoint = (baseUrl, key) => (path, params = {}) => `${baseUrl}/${path}?${qs.stringify({ ...params, apikey: key })}`;
+
+export const APICall = (endpoint, cb = null) => {
+
+  var req = http.get(endpoint);
+
+  if (!cb) {
+    return new Promise((resolve, reject) => {
+
+      req.end((error, { text }) => {
+
+        if (error) {
+          reject(error);
+        }
+        else {
+
+          const { parsingError, parsedResponse } = parseResponse(text);
+
+          if (parsingError) {
+            reject(parsingError);
+          }
+          else {
+            resolve(parsedResponse);
+          }
+        }
+      });
+    });
+  }
+  else {
+    return req.end((error, { text }) => {
+
+      if (error) {
+        cb(error);
+      }
+      else {
+        const { parsingError, parsedResponse } = parseResponse(text);
+
+        cb(parsingError, parsedResponse);
+      }
+    });
+  }
+};
+
+const parseResponse = (text) => {
+
+  let parsingError = null;
+  let parsedResponse = null;
+
+  try {
+    parsedResponse = JSON.parse(text);
+  }
+  catch(e) {
+    error = e;
+  }
+
+  return { parsingError, parsedResponse };
+}
---
+++ src/main.js
@@ -0,0 +1,51 @@
+import { buildEndpoint, APICall } from './helpers';
+
+const apiBaseURL = 'http://api.rottentomatoes.com/api/public/v1.0';
+
+module.exports = class RottenTomatoes {
+
+  constructor(config = {}) {
+
+    if (!config.hasOwnProperty('apiKey')) {
+      throw new Error('RottenTomatoes: apiKey needs to be provided.');
+    }
+
+    this.config = config;
+    this.buildURL = buildEndpoint(apiBaseURL, this.config.apiKey);
+  }
+
+  get list() {
+    return {
+      movies: {
+        boxOffice:  (params, cb) => APICall(this.buildURL('lists/movies/box_office.json', params), cb),
+        inTheaters: (params, cb) => APICall(this.buildURL('lists/movies/in_theaters.json', params), cb),
+        opening:    (params, cb) => APICall(this.buildURL('lists/movies/opening.json', params), cb),
+        upcoming:   (params, cb) => APICall(this.buildURL('lists/movies/upcoming.json', params), cb)
+      },
+      dvds: {
+        topRentals:      (params, cb) => APICall(this.buildURL('lists/dvds/top_rentals.json', params), cb),
+        currentReleases: (params, cb) => APICall(this.buildURL('lists/dvds/current_releases.json', params), cb),
+        newReleases:     (params, cb) => APICall(this.buildURL('lists/dvds/new_releases.json', params), cb),
+        upcoming:        (params, cb) => APICall(this.buildURL('lists/dvds/upcoming.json', params), cb)
+      }
+    }
+  }
+
+  movie(movieId) {
+
+    if (!movieId) {
+      throw new Error('RottenTomatoes: invalid movie ID.');
+    }
+
+    const slugStart = `movies/${movieId}`;
+
+    return {
+      infos:   (cb) => APICall(this.buildURL(`${slugStart}.jsn`), cb),
+      cast:    (cb) => APICall(this.buildURL(`${slugStart}/cast.json`), cb),
+      clips:   (cb) => APICall(this.buildURL(`${slugStart}/clips.json`), cb),
+      reviews: (cb) => APICall(this.buildURL(`${slugStart}/reviews.json`), cb),
+      similar: (cb) => APICall(this.buildURL(`${slugStart}/similar.json`), cb),
+      alias:   (cb) => APICall(this.buildURL(`${slugStart}/movie_alias.json`), cb)
+    }
+  }
+}
