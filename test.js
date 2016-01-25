3f893baa802a991755fc2529182167078b21dbc7
Julien Viala <dev.vialajulien@gmail.com>
2016-01-25 20:17:19 +0100

    remove untracked folder

--- test.js
+++ test.js
@@ -2,6 +2,8 @@ import test from 'ava';
 import 'babel-core/register';
 import R from './src';
 
+var rt = new R();
+
 test('class R should have static method init', t => t.ok(R.init))
 test('class R static method init should be callable', t => t.true(typeof R.init === "function"))
 test('class R static method init has one argument', t => t.true(R.init.length === 1))
