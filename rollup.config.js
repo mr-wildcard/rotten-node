8ad4f661340891fbe211f82933b163537cacac23
Julien Viala <dev.vialajulien@gmail.com>
2016-01-31 17:12:47 +0100

    put rollup bundling js into its own file
    this allow to use babel with a separate config and let .babelrc file be
    used by AVA

--- rollup.config.js
+++
@@ -1,8 +0,0 @@
-const babel = require('rollup-plugin-babel');
-
-export default {
-  entry: 'src/main.js',
-  dest: 'lib/index.js',
-  plugins: [ babel() ],
-  format: 'umd'
-};
---
+++ rollup.js
@@ -0,0 +1,16 @@
+import { rollup } from 'rollup';
+import babel from 'rollup-plugin-babel';
+
+rollup({
+  entry: 'src/main.js',
+  dest: 'lib/index.js',
+  plugins: [ babel({
+    runtimeHelpers: true,
+    presets: [
+      "stage-0",
+      "es2015-rollup"
+    ]
+  }) ],
+  format: 'umd'
+})
+.then(console.log('Sources bundled !'));
